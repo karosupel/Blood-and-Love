1aa3f26227d0158558a50f6dca3cb6a972d7db36
Alexander <mystique.exarr@gmail.com>
2026-04-07 22:59:37 +0200

    Revert "Add RoomManager, transitions, tilesets & scenes"
    This reverts commit d09db8695a3ea1b80c29f2b8d1321973fa631562, reversing
    changes made to 1b083b405c9cd7708432dc472e6c11aea7a9ec34.

--- Assets/szyymkuus/Scripts/Interfaces/IDamageable.cs
+++
@@ -1,9 +0,0 @@
-
-public interface IDamageable
-{
-    float MaxHealth { get; }
-    float CurrentHealth { get; }
-    void TakeDamage(float damage);
-    void Heal(float amount);
-    void Die();
-}
