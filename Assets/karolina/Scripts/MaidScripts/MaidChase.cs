e8d70a0dab60ea151c1f22ec1b5d82a1f90e234c
karosupel <karosupel@gmail.com>
2026-04-10 00:51:40 +0200

    Maid rotated and sprite added

--- Assets/karolina/Scripts/MaidScripts/MaidChase.cs
+++ Assets/karolina/Scripts/MaidScripts/MaidChase.cs
@@ -21,7 +21,15 @@ public class MaidChase : MaidBaseState
             return;
         }
         Vector2 direction = (player.transform.position - enemy.transform.position).normalized;
-        enemy.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        //enemy.transform.rotation = Quaternion.LookRotation(Vector3.forward, direction);
+        if(enemy.transform.position.x < player.transform.position.x)
+        {
+            enemy.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else
+        {
+            enemy.transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
         enemy.transform.position += (Vector3)direction * Time.deltaTime * enemy.stats.moveSpeed;
 
         if (Vector2.Distance(enemy.transform.position, player.transform.position) < enemy.stats.attackRange)
